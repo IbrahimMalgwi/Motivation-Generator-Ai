38b9d7e0adb83a0516370db14a7458c8615a1b0f
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2023-06-06 14:34:37 +0100

    MotivationGenrator AI Application

---
+++ README.md
@@ -0,0 +1,6 @@
+# AI Motivation Generator powered by GPT-3
+
+Full-stack application built with NextJS, API routes, and OpenAI's GPT-3 API.
+
+To run the app, you need to add a `.env.local` file to the project root and add your [OpenAI API key](https://openai.com/api/) under `OPENAI_API_KEY`.
+
---
+++ pages/index.tsx
@@ -0,0 +1,77 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+import mainImage from '@/assets/images/main.jpg'
+import { Form, Button, Spinner } from 'react-bootstrap'
+import { FormEvent, useState } from 'react'
+
+export default function Home() {
+
+  const [quote, setQuote] = useState("");
+  const [quoteLoading, setQuoteLoading] = useState(false);
+  const [quoteLoadingError, setQuoteLoadingError] = useState(false);
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const prompt = formData.get("prompt")?.toString().trim();
+
+    if (prompt) {
+      try {
+        setQuote("");
+        setQuoteLoadingError(false);
+        setQuoteLoading(true);
+
+        const response = await fetch(`https://api/generate?prompt=${encodeURIComponent(prompt)}`);
+        const body = await response.json();
+        setQuote(body.quote);
+      } catch (error) {
+        console.error(error);
+        setQuoteLoadingError(true);
+      } finally {
+        setQuoteLoading(false);
+      }
+    }
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Motivation Generator AI - Generate Text</title>
+        <meta name="description" content="by Coding in Flow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1>Motivation Generator AI</h1>
+        <h2>powered by GPT-3</h2>
+        <div>Enter a topic and the AI will Generate Motivation text for you</div>
+        <div className={styles.mainImageContainer}>
+          <Image
+            src={mainImage}
+            fill
+            alt='A picture of a Ibrahim smiling'
+            priority
+            className={styles.mainImage}
+          />
+        </div>
+        <Form onSubmit={handleSubmit} className={styles.inputForm}>
+          <Form.Group className='mb-3' controlId='prompt-input'>
+            <Form.Label>Generate Motivation About about...</Form.Label>
+            <Form.Control
+              name='prompt'
+              placeholder='e.g. lifestyle, programming, people'
+              maxLength={100}
+            />
+          </Form.Group>
+          <Button type='submit' className='mb-3' disabled={quoteLoading}>
+            Generate Motivation
+          </Button>
+        </Form>
+        {quoteLoading && <Spinner animation='border' />}
+        {quoteLoadingError && "Something went wrong. Please try again."}
+        {quote && <h5>{quote}</h5>}
+      </main>
+    </>
+  )
+}
